1af331b4ae1515d66bb128541143e1a9b94ca0aa
Hugo Portela <hugoportela0611@gmail.com>
2024-10-23 15:23:03 -0300

    Inserindo o Projeto no Github

---
+++ tir/CRMA980TESTSUITE.py
@@ -0,0 +1,14 @@
+import unittest
+
+from CRMA980TESTCASE import CRMA980
+
+suite = unittest.TestSuite()
+
+suite.addTest(CRMA980('test_CRMA980_CT133'))
+
+runner = unittest.TextTestRunner(verbosity=2)
+result = runner.run(suite)
+
+if len(result.errors) > 0 or len(result.failures) > 0:
+    print("custom exit")
+    exit(1)
